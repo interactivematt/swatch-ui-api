a154571d9d67d5613843bc422802ad8e85c6f055
Matt Castillo <mattcastillo2010@gmail.com>
2021-01-31 20:42:19 -0600

    Getting ready for Heroku

---
+++ seeds/seeds.swatches.sql
@@ -0,0 +1,7 @@
+INSERT INTO swatches (name, color_primary, color_secondary, font_primary)
+VALUES
+  ('Colorful', '#223222', '#e2c223', 'Open Sans'),
+  ('Stark', '#333333', '#BBBBBB', 'Rubik'),
+  ('Fresh', '#6AA84F', '#BCBC3E', 'Work Sans'),
+  ('Clean', '#564074', '#000218', 'Source Code Pro'),
+  ('Calm', '#9962BA', '#C3A4E0', 'Merriweather');
--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  API_BASE_URL: process.env.REACT_APP_API_BASE_URL ||
+  DATABASE_URL: process.env.REACT_APP_DATABASE_URL ||
     "http://localhost:8000/api",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 }
