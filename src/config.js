5033ea133e477a65a3b3ab941fa9ecd378ba0338
Matt Castillo <mattcastillo2010@gmail.com>
2021-01-31 22:11:17 -0600

    Stupid heroku

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
   API_TOKEN: process.env.API_TOKEN,
-  DATABASE_URL: process.env.REACT_APP_DATABASE_URL ||
-    "http://localhost:8000/api",
+  DATABASE_URL: process.env.DATABASE_URL ||
+    "postgresql://postgres@localhost/swatch-ui",
   CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://swatch-ui.vercel.app/'
 }
